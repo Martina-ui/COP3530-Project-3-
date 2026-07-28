0fbe5129d3ac11413a649d6b734c9a7635d30e3e
Martina Delger <martidelger@gmail.com>
2026-07-28 18:20:44 +0000

    added required functions to header file

--- src/CampusCompass.h
+++ src/CampusCompass.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
@@ -19,7 +20,7 @@ struct ClassInfo {
 
 struct Student {
     string name;
-    string student_id;
+    string student_ID;
     int residence_location_id;
     vector<string> class_codes;
 };
@@ -33,4 +34,14 @@ public:
     CampusCompass(); // constructor
     bool ParseCSV(const string &edges_filepath, const string &classes_filepath);
     bool ParseCommand(const string &command);
+    void insert_student(const string& name, const string& student_ID, int residence_location_id, const vector<string>& class_codes);
+    void remove_student(const string& student_ID);
+    void drop_class(const string& student_ID, const string& class_code);
+    void replace_class(const string& student_ID, const string& old_class_code, const string& new_class_code);
+    void remove_class(const string& class_code);
+    void toggle_edges_closure(const vector<int>& location_ids);
+    void check_edge_status(int location_x, int location_y); 
+    void is_connected(int location_1, int location_2);
+    void print_shortest_edges(const string& student_ID); 
+    void print_student_zone(const string& student_ID); 
 };
